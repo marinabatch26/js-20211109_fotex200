973f23274fc2adfce2350dd1e48955e105f17996
apushkarou <53324171+apushkarou@users.noreply.github.com>
2021-11-25 12:42:16 -0500

    Column chart update

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,44 +1,71 @@
 export default class ColumnChart {
-  constructor(options = {}) {
-    this.options = options;
+  constructor({
+    data = [],
+    label = '',
+    link = '',
+    value = 0,
+    formatHeading = (value) => value
+  } = {}) {
+    this.data = data;
+    this.label = label;
+    this.link = link;
+    this.value = value;
+    this.formatHeading = formatHeading(value);
     this.chartHeight = 50;
     this.render();
   }
 
-  render() {
-    const { data = [], label, value, link, formatHeading = (value) => value} = this.options;
-    const loading = (data.length === 0) ? true : false;
-
-    const element = document.createElement('div');
-    element.className = `${loading ? 'column-chart column-chart_loading' : 'column-chart'}`
-    element.innerHTML = `
+  get template() {
+    return `
+      <div class="column-chart column-chart_loading">
         <div id="title" class="column-chart__title">
-          Total ${label ? label : ''}
-          ${link ? this.renderLink(link) : ''}
+          Total ${this.label}
+          ${this.renderLink()}
         </div>
         <div class="column-chart__container">
-          <div data-element="header" class="column-chart__header">${formatHeading(value)}</div>
-          <div data-element="body" class="column-chart__chart"></div>
+          <div data-element="header" class="column-chart__header">${this.formatHeading}</div>
+          <div data-element="body" class="column-chart__chart">
+            ${this.renderBody(this.data)}
+          </div>
         </div>
+      </div>
     `;
+  }
 
-    this.element = element;
-    this.renderBody(data);
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+
+    if (this.data.length) {
+      this.element.classList.remove('column-chart_loading');
+    }
+
+    this.subElements = this.getSubElements(this.element);
+  }
+
+  getSubElements(element) {
+    const result = {};
+    const elements = element.querySelectorAll('[data-element]');
+
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+
+      result[name] = subElement;
+    }
+
+    return result;
   }
 
   renderBody(data) {
     const columnProps = this.getColumnProps(data);
-    const chartBodyHtml = data.reduce((acc, current, index) => {
-      return (
-        acc + `<div style="--value: ${columnProps[index].value}" data-tooltip="${columnProps[index].percent}"></div>`
-      )
-    }, '');
-
-    this.element.querySelector('.column-chart__chart').insertAdjacentHTML('afterbegin', chartBodyHtml);
+    return data.map((item, index) => {
+      return item = `<div style="--value: ${columnProps[index].value}" data-tooltip="${columnProps[index].percent}"></div>`;
+    }).join('');
   }
 
-  renderLink(link) {
-    return `<a href="${link}" class="column-chart__link">View All</a>`
+  renderLink() {
+    return this.link ? `<a href="${this.link}" class="column-chart__link">View All</a>` : '';
   }
 
   getColumnProps(data) {
@@ -53,14 +80,12 @@ export default class ColumnChart {
     });
   }
 
-  update(newData) {
-    this.element.querySelector('.column-chart__chart').innerHTML = '';
-    this.renderBody(newData);
+  update(data) {
+    this.subElements.body.innerHTML = this.renderBody(data);
   }
 
   destroy() {
-    return this.element.remove();
-    // ? тесты требовали, возможно что то не дописал, но вроде как работает
+    return this.remove();
   }
 
   remove() {
